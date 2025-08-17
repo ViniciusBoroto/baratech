de262b8bf4e4366072080029e7326b3fefd752f4
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2025-08-17 02:25:20 -0300

    refact: core usecase tests

--- internal/usecase/core_usecase_test.go
+++ internal/usecase/core_usecase_test.go
@@ -12,6 +12,50 @@ import (
 func TestCoreUsecase_FindBestCore(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
+	type testMocks struct {
+		CpuRepo *mocks.MockCpuRepository
+	}
+
+	tt := []struct {
+		name          string
+		budget        float64
+		setMocks      func(m *testMocks)
+		expectedCore  domain.Core
+		expectedError error
+	}{
+		{
+			name:   "Sufficient budget",
+			budget: 1000,
+			setMocks: func(m *testMocks) {
+				m.CpuRepo.EXPECT().AllOrderByScoreDesc().Return([]domain.Cpu{
+					{Cores: 6, Speed: 4.0, Socket: "LGA1200", Score: 1500, Price: 300.00},
+					{Cores: 4, Speed: 3.5, Socket: "AM4", Score: 1000, Price: 200.00},
+					{Cores: 4, Speed: 3.5, Socket: "AM4", Score: 800, Price: 250.00},
+				}, nil).Times(1)
+			},
+			expectedCore: domain.Core{
+				Cpu:  domain.Cpu{Cores: 6, Speed: 4.0, Socket: "LGA1200", Score: 1500, Price: 300.00},
+				Mobo: domain.Mobo{Socket: "LGA1200", Price: 300.00},
+			},
+			expectedError: nil,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			cpuRepo := mocks.NewMockCpuRepository(ctrl)
+			usecase := NewCoreUsecase(cpuRepo)
+			tc.setMocks(&testMocks{CpuRepo: cpuRepo})
+
+			result, err := usecase.FindBestCore(tc.budget)
+			assert.Equal(t, tc.expectedError, err)
+			assert.Equal(t, tc.expectedCore, result)
+		})
+	}
+}
+
+func TestCoreUsecase_FindBestCore_WhenInsufficientBudget(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 	cpuRepo := mocks.NewMockCpuRepository(ctrl)
 	usecase := NewCoreUsecase(cpuRepo)
 
